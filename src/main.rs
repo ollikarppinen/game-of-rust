93c99df7a88f98ed75a37487469944d5ae7457f0
Olli Karppinen <ollikarppinen@gmail.com>
2023-11-25 16:39:22 +0200

    Adds infinite grid

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
-use rand::Rng;
 use sdl2::{pixels::Color, render::Canvas};
 use sdl2::event::Event;
 use sdl2::keyboard::{Keycode, self};
 use sdl2::rect::{Rect, Point};
 use sdl2::video::Window;
+use std::collections::HashSet;
+use std::fmt;
 use std::fs::File;
 use std::io::{BufReader, BufRead};
-use std::path::Path;
 use std::time::Instant;
 
 const WINDOW_WIDTH: u32 = 800;
@@ -14,11 +14,8 @@ const WINDOW_HEIGHT: u32 = 600;
 const MS_PER_UPDATE: f32 = 100.0;
 const CELL_WIDTH: u32 = 10;
 const CELL_HEIGHT: u32 = 10;
-const GRID_WIDTH_IN_CELLS: u32 = 50;
-const GRID_HEIGHT_IN_CELLS: u32 = 50;
-const GRID_SIZE: u32 = GRID_WIDTH_IN_CELLS * GRID_HEIGHT_IN_CELLS;
-const INITIAL_X_OFFSET: i32 = ((WINDOW_WIDTH - GRID_WIDTH_IN_CELLS * CELL_WIDTH) / 2) as i32;
-const INITIAL_Y_OFFSET: i32 = ((WINDOW_HEIGHT - GRID_HEIGHT_IN_CELLS * CELL_HEIGHT) / 2) as i32;
+const INITIAL_X_OFFSET: i32 = 0;
+const INITIAL_Y_OFFSET: i32 = 0;
 
 #[derive(Debug)]
 pub struct Config {
@@ -26,12 +23,7 @@ pub struct Config {
     window_height: u32,
     dt: f32,
     cell_width: u32,
-    cell_height: u32,
-    grid_width_in_cells: u32,
-    grid_height_in_cells: u32,
-    grid_width: u32,
-    grid_height: u32,
-    grid_size: u32
+    cell_height: u32
 }
 
 impl Config {
@@ -41,12 +33,7 @@ impl Config {
             window_height: WINDOW_HEIGHT,
             dt: MS_PER_UPDATE,
             cell_width: CELL_WIDTH,
-            cell_height: CELL_HEIGHT,
-            grid_width_in_cells: GRID_WIDTH_IN_CELLS,
-            grid_height_in_cells: GRID_HEIGHT_IN_CELLS,
-            grid_size: GRID_WIDTH_IN_CELLS * GRID_HEIGHT_IN_CELLS,
-            grid_width: GRID_WIDTH_IN_CELLS * CELL_WIDTH,
-            grid_height: GRID_HEIGHT_IN_CELLS * CELL_HEIGHT
+            cell_height: CELL_HEIGHT
         }
     }
 }
@@ -124,42 +111,43 @@ impl Game {
     
     pub fn initial_state(&mut self) -> State {
         let mut state = State::new();
-        let grid_width_in_cells = self.config.grid_width_in_cells;
-        let grid_height_in_cells = self.config.grid_height_in_cells;
-        let file_path = format!("./initial_cells/{grid_width_in_cells}_{grid_height_in_cells}.txt");
-        if Path::new(&file_path).exists() {
-            let file = File::open(file_path).expect("file wasn't found.");
-            let reader = BufReader::new(file);
+        // return state;
 
-            let numbers: Vec<usize> = reader
-                .lines()
-                .map(|line| line.unwrap().parse::<usize>().unwrap())
-                .collect();
+        let file = File::open("./initial_cells.txt").expect("file wasn't found.");
+        let reader = BufReader::new(file);
+        let mut coords: Vec<i32> = reader
+            .lines()
+            .map(|line| line.unwrap().parse::<i32>().unwrap())
+            .collect();
 
-            for i in numbers {
-                state.game_grid[i] = Some(Cell::new(Some(0.0)));
-            }
-        } else {
-            for _i in 0..(self.config.grid_size as f32 / 2.0).round() as i32 {
-                let i = rand::thread_rng().gen_range(0..self.config.grid_size);
-                state.game_grid[i as usize] = Some(Cell::new(Some(0.0)));
+        while !coords.is_empty() {
+            match (coords.pop(), coords.pop()) {
+                (Some(x), Some(y)) => {
+                    state.cell_coords.insert(Coord::new(x, y));
+                },
+                _ => {}
             }
         }
+
         state
     }
 
     pub fn integrate(&mut self, mut state: State, t: f32) -> State {
-        if self.event_pump.keyboard_state().is_scancode_pressed(keyboard::Scancode::Down) && self.offset_y > -(self.config.grid_height as i32) / 2 {
+        if self.event_pump.keyboard_state().is_scancode_pressed(keyboard::Scancode::Down) {
             self.offset_y -= 1;
+            println!("offset_y: {}", self.offset_y);
         }
-        if self.event_pump.keyboard_state().is_scancode_pressed(keyboard::Scancode::Up) && self.offset_y < (self.config.grid_height as i32) {
+        if self.event_pump.keyboard_state().is_scancode_pressed(keyboard::Scancode::Up) {
             self.offset_y += 1;
+            println!("offset_y: {}", self.offset_y);
         }
-        if self.event_pump.keyboard_state().is_scancode_pressed(keyboard::Scancode::Right) && self.offset_x > -(self.config.grid_width   as i32) / 2 {
+        if self.event_pump.keyboard_state().is_scancode_pressed(keyboard::Scancode::Right) {
             self.offset_x -= 1;
+            println!("offset_x: {}", self.offset_x);
         }
-        if self.event_pump.keyboard_state().is_scancode_pressed(keyboard::Scancode::Left) && self.offset_x < (self.config.grid_width     as i32) {
+        if self.event_pump.keyboard_state().is_scancode_pressed(keyboard::Scancode::Left) {
             self.offset_x += 1;
+            println!("offset_x: {}", self.offset_x);
         }
 
         for event in self.event_pump.poll_iter() {
@@ -180,35 +168,19 @@ impl Game {
                     println!("dt: {}", self.config.dt);
                 },
                 Event::KeyDown { keycode: Some(Keycode::R), .. } => {
-                    state.reset();
+                    // state.reset();
+                    self.offset_x = 0;
+                    self.offset_y = 0;
                 },
                 Event::KeyDown { keycode: Some(Keycode::P), .. } => {
-                    for i in 0..self.config.grid_size {
-                        match &state.game_grid[i as usize] {
-                            Some(_) => {
-                                println!("i: {}", i);
-                            },
-                            _ => {}
-                        }
-                    }
                 },
                 Event::MouseButtonDown { x, y, .. } => {
-                    let grid_i = Game::get_grid_i(x - self.offset_x, y - self.offset_y, &self.config);
-                    match &grid_i {
-                        Some(i) => {
-                            println!("mouse button down, i: {}", i);
-
-                            match state.game_grid[*i] {
-                                Some(_) => {
-                                    state.game_grid[*i] = None;
-                                },
-                                None => {
-                                    state.game_grid[*i] = Some(Cell::new(Some(t)));
-                                }
-                            }
-                        },
-                        _ => {}
-                    }
+                    let coord = Coord::new(
+                        ((x + self.offset_x) as f32 / self.config.cell_width as f32) as i32,
+                        ((y + self.offset_y) as f32 / self.config.cell_height as f32) as i32
+                    );
+                    println!("click coord: {}", coord);
+                    state.cell_coords.insert(coord);
                 },
                 _ => {}
             }
@@ -226,22 +198,16 @@ impl Game {
 
         let mut new_state = State::new();
 
-        for i in 0..GRID_SIZE {
-            new_state.game_grid[i as usize] = state.next(i as i32, t);
+        for cell_coord in &state.cell_coords {
+            if state.should_live(&cell_coord) { new_state.cell_coords.insert(cell_coord.clone()); }
+            // update_coords.push(cell_coord.clone());
+            for neighbor_coord in cell_coord.neighbors() {
+                // update_coords.push(neighbor_coord);
+                if state.should_live(&neighbor_coord) { new_state.cell_coords.insert(neighbor_coord.clone()); }
+            }
         }
 
         new_state
-    }
-
-    pub fn get_grid_i(x: i32, y: i32, config: &Config) -> Option<usize> {
-        if x >= 0 && y >= 0 && x < config.grid_width as i32 && y < config.grid_height as i32 {
-            let grid_x = x / config.cell_width as i32;
-            let grid_y = y / config.cell_height as i32;
-            let grid_i = grid_x + grid_y * config.grid_width_in_cells as i32;
-            return Some(grid_i as usize);
-        } else {
-            return None;
-        }
     }
 
     pub fn dt(&self) -> f32 {
@@ -263,187 +229,148 @@ impl Game {
     fn render_grid(&mut self) {
         self.canvas.set_draw_color(Color::BLACK);
 
-        self.canvas.draw_line(
-            Point::new(self.offset_x, self.offset_y),
-            Point::new(self.offset_x + self.config.grid_width as i32, self.offset_y)
-        ).expect("could not draw line");
-        self.canvas.draw_line(
-            Point::new(self.offset_x + self.config.grid_width as i32, self.offset_y),
-            Point::new(
-                self.offset_x + self.config.grid_width as i32,
-                self.offset_y + self.config.grid_height as i32
-            )
-        ).expect("could not draw line");
-
-        for i in 0..self.config.grid_height_in_cells {
+        let mut x = self.offset_x % self.config.cell_width as i32;
+        while x < self.config.window_width as i32 {
             self.canvas.draw_line(
                 Point::new(
-                    self.offset_x,
-                    self.offset_y + ((i + 1) * self.config.cell_height) as i32
+                    x,
+                    0
                 ),
                 Point::new(
-                    self.offset_x + self.config.grid_width as i32,
-                    self.offset_y + ((i + 1) * self.config.cell_height) as i32
+                    x,
+                    self.config.window_height as i32
                 )
             ).expect("could not draw line");
+            x += self.config.cell_width as i32;
         }
-        for i in 0..self.config.grid_width_in_cells {
+        let mut y = self.offset_y % self.config.cell_height as i32;
+        while y < self.config.window_height as i32 {
             self.canvas.draw_line(
                 Point::new(
-                    self.offset_x + (i * self.config.cell_width)  as i32,
-                    self.offset_y
+                    0,
+                    y
                 ),
                 Point::new(
-                    self.offset_x + (i * self.config.cell_width) as i32,
-                    self.offset_y + self.config.grid_height as i32
+                    self.config.window_width as i32,
+                    y
                 )
             ).expect("could not draw line");
+            y += self.config.cell_height as i32;
         }
     }
 
     fn render_state(&mut self, state: &State) {
-        for i in 0..self.config.grid_size {
-            if state.is_live(i as i32) {
-                let x = i % self.config.grid_width * self.config.cell_width;
-                let y = i / self.config.grid_width * self.config.cell_height;
-                self.render_live_cell(i);
-            }
+        for coord in &state.cell_coords {
+            self.render_live_cell(&coord);
         }
     }
 
     fn render_hover(&mut self) {
-        match Game::get_grid_i(self.event_pump.mouse_state().x() - self.offset_x, self.event_pump.mouse_state().y() - self.offset_y, &self.config) {
-            Some(i) => {
-                self.render_hover_cell(i as u32);
-            },
-            None => {}
-        };
+        let coord = Coord::new(
+            ((self.event_pump.mouse_state().x() + self.offset_x) as f32 / self.config.cell_width as f32) as i32,
+            ((self.event_pump.mouse_state().y() + self.offset_y) as f32 / self.config.cell_height as f32) as i32
+        );
+        self.render_hover_cell(coord);
     }
 
-    fn render_live_cell(&mut self, i: u32) {
-        let x = i % self.config.grid_width_in_cells * self.config.cell_width;
-        let y = i / self.config.grid_width_in_cells * self.config.cell_height;
+    fn render_live_cell(&mut self, coord: &Coord) {
+        let x = coord.x * self.config.cell_width as i32;
+        let y = coord.y * self.config.cell_height as i32;
         self.canvas.set_draw_color(Color::BLACK);
-        self.canvas.fill_rect(Rect::new(x as i32 + self.offset_x, y as i32 + self.offset_y, self.config.cell_width, self.config.cell_height)).expect("could not fill rect");
+        self.canvas.fill_rect(Rect::new(x + self.offset_x, y + self.offset_y, self.config.cell_width, self.config.cell_height)).expect("could not fill rect");
     }
 
-    fn render_hover_cell(&mut self, i: u32) {
-        let x = i as u32 % self.config.grid_width_in_cells * self.config.cell_width;
-        let y = i as u32 / self.config.grid_width_in_cells * self.config.cell_height;
+    fn render_hover_cell(&mut self, coord: Coord) {
+        let x = coord.x * self.config.cell_width as i32;
+        let y = coord.y * self.config.cell_height as i32;
         self.canvas.set_draw_color(Color::GRAY);
         self.canvas.fill_rect(Rect::new(x as i32 + self.offset_x, y as i32 + self.offset_y, self.config.cell_width, self.config.cell_height)).expect("could not fill rect");
     }
 }
 
-
-pub struct Cell {
-    live: Option<f32>
+#[derive(Hash)]
+pub struct Coord {
+    x: i32,
+    y: i32
 }
 
-impl Cell {
-    pub fn new(live: Option<f32>) -> Cell {
-        Cell {
-            live: live
+impl Coord {
+    pub fn new(x: i32, y: i32) -> Coord {
+        Coord {
+            x: x,
+            y: y
         }
+    }
+
+    pub fn neighbors(&self) -> Vec<Coord> {
+        let mut coords = Vec::new();
+        coords.push(Coord::new(self.x, self.y + 1));
+        coords.push(Coord::new(self.x, self.y - 1));
+        coords.push(Coord::new(self.x + 1, self.y));
+        coords.push(Coord::new(self.x + 1, self.y + 1));
+        coords.push(Coord::new(self.x + 1, self.y - 1));
+        coords.push(Coord::new(self.x - 1, self.y));
+        coords.push(Coord::new(self.x - 1, self.y + 1));
+        coords.push(Coord::new(self.x - 1, self.y - 1));
+        coords
+    }
+}
+
+impl fmt::Display for Coord {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        // Use `self.number` to refer to each positional data point.
+        write!(f, "({}, {})", self.x, self.y)
+    }
+}
+
+impl PartialEq for Coord {
+    fn eq(&self, other: &Self) -> bool {
+        self.x == other.x && self.y == other.y
+    }
+}
+impl Eq for Coord {}
+
+impl Copy for Coord {}
+
+impl Clone for Coord {
+    fn clone(&self) -> Self {
+        Coord::new(self.x, self.y)
     }
 }
 
 pub struct State {
-    game_grid: [Option<Cell>; GRID_SIZE as usize]
+    cell_coords: HashSet<Coord>
 }
 
 impl State {
     pub fn new() -> State {
         State {
-            game_grid: std::array::from_fn(|_| None)
+            cell_coords: HashSet::new()
         }
     }
 
     pub fn reset(&mut self) {
-        self.game_grid = std::array::from_fn(|_| None);
+        self.cell_coords = HashSet::new();
     }
 
-    pub fn is_live(&self, i: i32) -> bool {
-        match &self.game_grid[i as usize] {
-            Some(Cell { live: Some(_), .. } ) => { true },
-            _ => { false }
-        }
+    pub fn is_live(&self, coord: &Coord) -> bool {
+        return self.cell_coords.contains(&coord);
     }
 
-    pub fn should_live(&self, i: i32) -> bool {
-        match self.neighbor_count(i) {
-            2 => { self.is_live(i) },
+    pub fn should_live(&self, coord: &Coord) -> bool {
+        match self.neighbor_count(&coord) {
+            2 => { self.is_live(&coord) },
             3 => { true },
             _ => { false }
         }
     }
 
-    pub fn get_live(&self, i: i32) -> Option<f32> {
-        match &self.game_grid[i as usize] {
-            Some(Cell { live: Some(t), .. } ) => {
-                return Some(*t);
-            },
-            _ => { None }
-        }
-    }
-
-    pub fn next(&self, i: i32, t: f32) -> Option<Cell> {
-        if self.should_live(i) {
-            return Some(
-                Cell::new(self.get_live(i).or(Some(t)))
-            );
-        } else {
-            None
-        }
-    }
-
-    pub fn neighbor_count(&self, i: i32) -> u8 {
+    pub fn neighbor_count(&self, coord: &Coord) -> u8 {
         let mut count = 0;
 
-        let grid_width = GRID_WIDTH_IN_CELLS as i32;
-        let grid_height = GRID_HEIGHT_IN_CELLS as i32;
-        let grid_size = grid_width * grid_height;
-
-        let top = i / grid_width == 0;
-        let bottom = i / grid_width >= grid_height - 1;
-        let left = i % grid_width == 0;
-        let right = i % grid_width == grid_width - 1;
-
-        let mut ii = i - 1;
-        if left { ii += grid_width }
-        if self.is_live(ii){ count += 1 }
-
-        ii = i + 1;
-        if right { ii -= grid_width }
-        if self.is_live(ii) { count += 1 }
-
-        ii = i - grid_width - 1;
-        if left { ii += grid_width }
-        if top { ii += grid_size }
-        if self.is_live(ii) { count += 1 }
-
-        ii = i - grid_width;
-        if top { ii += grid_size }
-        if self.is_live(ii) { count += 1 }
-
-        ii = i - grid_width + 1;
-        if right { ii -= grid_width }
-        if top { ii += grid_size }
-        if self.is_live(ii) { count += 1 }
-
-        ii = i + grid_width - 1;
-        if left { ii += grid_width }
-        if bottom { ii -= grid_size }
-        if self.is_live(ii) { count += 1 }
-
-        ii = i + grid_width;
-        if bottom { ii -= grid_size }
-        if self.is_live(ii) { count += 1 }
-
-        ii = i + grid_width + 1;
-        if right { ii -= grid_width }
-        if bottom { ii -= grid_size }
-        if self.is_live(ii) { count += 1 }
+        for coord in coord.neighbors() {
+            if self.is_live(&coord) { count += 1 }
+        }
 
         count
     }
@@ -468,10 +395,8 @@ fn main() -> Result<(), String> {
 
     // https://gafferongames.com/post/fix_your_timestep/
     let mut t: f32 = 0.0;
-
     let mut timestep = TimeStep::new();
     let mut accumulator = -1000.0;
-
 
     while game.running {
         let frame_time = timestep.delta();
